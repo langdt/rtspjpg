077aed0ac389f7322aeaf07035e3aa2f2e0de65c
Tony Langdon <tony@langdt.co.uk>
2022-01-16 12:43:07 +0000

    Use CAMn_RTSP_URL to config

--- server.js
+++ server.js
@@ -1,59 +1,83 @@
-const { exec } = require('child_process');
-const fs = require('fs');
-const path = require('path');
-const express = require('express');
+const { exec } = require("child_process");
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
 const app = express();
+const process = require("process");
 
-async function getImage(camera) {
-    const tmpfile = `images/cam${camera}.tmp.jpg`;
-    const imgfile = `images/cam${camera}.jpg`
-    const command = `ffmpeg -y -rtsp_transport tcp -i 'rtsp://admin:pass@192.168.1.50:554/cam/realmonitor?channel=${camera}&subtype=0'` +
-    ` -frames:v 1 images/cam${camera}.tmp.jpg`;
-    return new Promise((resolve, reject) => {
-        exec(command, (error, stdout, stderr) => {
-            if (error) {
-                reject(error);
-            }
-            else {
-                fs.rename(tmpfile, imgfile, (err) => {
-                    if(err) {
-                        reject(err);
-                    }
-                    else {
-                        console.log(`Updated ${imgfile}`);
-                        resolve(true);
-                    }
-                });
-            }
-        })
-    });
+const camKeys = getCameraURLs();
+if (camKeys.length == 0) {
+    console.log('No cameras configured through $CAM1_RTSP_URL etc');
+    process.exit(1);
 }
 
-const dir = path.join(__dirname, 'images');
-console.log(dir);
-app.use(express.static(dir));
-app.listen(3000, () => {
-    console.log('Listening on http://localhost:3000');
-});
-
 (async function main() {
-    createImagesDir();
-    setInterval(async () => {
-        try {
-            const fetches = new Array();
-            for(let i=1 ; i <=3 ; i++) {
-                fetches.push(await getImage(i));
-            }
-            await Promise.all(fetches);
-        }
-        catch(error){
-            console.error(error);
-        }
-    }, 5000);
+  createImagesDir();
+  startExpress();
+  setInterval(async () => {
+    try {
+        const fetches2 = camKeys.map(async (val, index) => {
+            await getImage(index, val)
+        })
+    //   const fetches = new Array();
+    //   for (let i = 1; i <= 3; i++) {
+    //     fetches.push(await getImage(i));
+    //   }
+      await Promise.all(fetches2);
+    } catch (error) {
+      console.error(error);
+    }
+  }, 5000);
 })();
 
 function createImagesDir() {
-    if(!fs.existsSync('images')) {
-        fs.mkdirSync('images');
-    }
+  if (!fs.existsSync("images")) {
+    fs.mkdirSync("images");
+  }
 }
+
+function getCameraURLs() {
+  const env = process.env;
+  const camKeys = Object.keys(env).filter((key) =>
+    /^CAM\d+_RTSP_URL/.test(key)
+  );
+  const camURLS = [];
+  camKeys.forEach((value) => {
+    const matches = value.match(/^CAM(\d+)_RTSP_URL/);
+    if (matches) {
+      camURLS[matches[1]] = env[value];
+    }
+  });
+  return camURLS;
+}
+
+function startExpress() {
+  const dir = path.join(__dirname, "images");
+  app.use(express.static(dir));
+  app.listen(3000, () => {
+    console.log("Listening on http://localhost:3000");
+  });
+}
+
+async function getImage(camera, url) {
+    const tmpfile = `images/cam${camera}.tmp.jpg`;
+    const imgfile = `images/cam${camera}.jpg`;
+    const command =
+      `ffmpeg -y -rtsp_transport tcp -i '${url}' -frames:v 1 ${tmpfile}`;
+    return new Promise((resolve, reject) => {
+      exec(command, (error, stdout, stderr) => {
+        if (error) {
+          reject(error);
+        } else {
+          fs.rename(tmpfile, imgfile, (err) => {
+            if (err) {
+              reject(err);
+            } else {
+              console.log(`Updated ${imgfile}`);
+              resolve(true);
+            }
+          });
+        }
+      });
+    });
+  }
